a84f8e4cf075dc78be0a87d12ace69484ccc8994
deadragons <48061756+deadragons@users.noreply.github.com>
2021-10-21 11:05:49 -0500

    Correción de errores
    errores al conectar con practica 2

--- integracion/Function.Integrations/Function.Integrations/Settings.cs
+++ integracion/Function.Integrations/Function.Integrations/Settings.cs
@@ -1,9 +1,14 @@
-﻿namespace Function.Integrations
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Function.Integrations
 {
-    public class Settings
+   public class Settings
     {
-        public string DotnetExecutablePath { get; set; }
+        public string DotnetExPath { get; set; }
         public string FunctionHostPath { get; set; }
         public string FunctionApplicationPath { get; set; }
+        public string AzureWebJobsStorage { get; set; }
     }
 }
